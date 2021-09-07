72604e8f8728f5bee569378ed502746265ef7e3e
Rjan <rajandhakal1234@gmail.com>
2021-09-07 06:52:28 +0100

    js

---
+++ js/mypage.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('#let').click(function(){
+        $('.all').slideToggle(1000)
+    });
+    // $('#mode').click(function(){
+    //     $('.body1').toggleClass('dark')
+    //   });
+    $('#mode').click(function(){
+        $('.body1').toggleClass('dark')
+ 
+    });
+    
+});
